8ce4d4382f0d901065abd91c4c15b77c4fde5bfb
Bullseye665 <158263152+Bullseye665@users.noreply.github.com>
2024-06-09 14:56:08 -0400

    Update default.nix

--- modules/xfce/default.nix
+++ modules/xfce/default.nix
@@ -1,1 +1,15 @@
- 
+{ config, pkgs, callPackage, ... }: {
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+      xfce = {
+        enable = true;
+        noDesktop = true;
+        enableXfwm = false;
+      };
+    };
+    displayManager.defaultSession = "xfce";
+    windowManager.i3.enable = true;
+  };
+} 
