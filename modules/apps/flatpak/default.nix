804fef718f0fc61f632c1d980582dc653972baa9
Bullseye665 <158263152+Bullseye665@users.noreply.github.com>
2024-06-09 08:29:35 -0400

    Add files via upload

---
+++ modules/apps/flatpak/default.nix
@@ -0,0 +1,49 @@
+{ nix-flatpak, ... }: {
+  imports = [ nix-flatpak.nixosModules.nix-flatpak ];
+
+  services.flatpak = {
+    packages = [
+     "org.qbittorrent.qBittorrent"  # Torrent  -- flood
+     "com.ktechpit.torrhunt"        # Torrent Search
+
+      "com.dosbox_x.DOSBox-X"       # DOS Box
+      "org.duckstation.DuckStation" # PS1
+      "net.pcsx2.PCSX2"             # PS2
+      "net.rpcs3.RPCS3"             # PS3
+     # "app.xemu.xemu"               # Xbox
+      "io.mgba.mGBA"                # GBA
+      "org.desmume.DeSmuME"         # DS
+      "net.kuribo64.melonDS"        # DS
+      "io.github.simple64.simple64" # N64
+      "org.DolphinEmu.dolphin-emu"  # Wii and GC
+      "info.cemu.Cemu"              # Wii U
+      "org.ryujinx.Ryujinx"         # Switch
+      "org.libretro.RetroArch"      #
+
+     # "com.heroicgameslauncher.hgl" # Epic, GOG, and AP Launcher
+
+#      "com.github.Xenoveritas.abuse"             # Abuse 1995
+#      "io.gitlab.stone_kingdoms.StoneKingdoms"   # Stronghold
+#      "io.github.theforceengine.tfe"             # STAR WARS: Dark Forces
+#      "org.tuxemon.Tuxemon"                      # Dif Pokemon
+#      "com.endlessnetwork.dragonsapprentice"     # Dragon’s Apprentice
+#      "net.sourceforge.uhexen2.uhexen2"          # Hexen II
+     # "com.endlessnetwork.passage"               # The Passage
+#      "org.diasurgical.DevilutionX"              # Diablo
+      "com.exok.Celeste64"                        # Celeste 64
+
+    ];
+
+    update.auto = {
+      enable = true;
+      onCalendar = "weekly"; # Default value
+    };
+
+#    overrides = {
+#      global = {
+#        # Force correct theme for some GTK apps
+#        GTK_THEME = "Adwaita:dark";
+#       };
+#    };
+  };
+}
