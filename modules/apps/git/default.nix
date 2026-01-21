e9b11d36eb247066f83fb23f17db7215c0727d9b
Bullseye665 <158263152+Bullseye665@users.noreply.github.com>
2026-01-21 07:48:45 -0500

    Update default.nix

--- modules/apps/git/default.nix
+++ modules/apps/git/default.nix
@@ -6,8 +6,8 @@
     wget
   ];
 
-#  programs.git = {
-#    enable = true;
+  programs.git = {
+    enable = true;
 #    userName = "Bullseye665";
 #    userEmail = "caleb.comstock6@gmail.com";
 #  };
